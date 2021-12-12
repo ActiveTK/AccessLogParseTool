65cda8efa1c6c8117792c7f6f46b32a2eeb2760f
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2021-12-12 11:06:45 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja" itemscope="" itemtype="http://schema.org/WebPage" dir="ltr">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+    <title>アクセスログ解析ツール</title>
+    <meta name="author" content="ActiveTK.">
+    <meta name="robots" content="noindex">
+    <meta name="favicon" content="https://www.activetk.cf/icon/index_32_32.ico">
+    <style>a{color: #00ff00;position: relative;display: inline-block;transition: .3s;}a::after {position: absolute;bottom: 0;left: 50%;content: '';width: 0;height: 2px;background-color: #31aae2;transition: .3s;transform: translateX(-50%);}a:hover::after{width: 100%;}</style>
+  </head>
+  <body style="background-color:#e6e6fa;text:#363636;">
+    <br>
+    <div align='center'>
+      <h2>アクセスログ解析ツール</h2>
+      <hr color="#363636" size="2"><br>
+      <form action='run.php' method='POST' enctype="multipart/form-data">
+        <input type='file' name='logfile' required><br><br>
+        <input type='submit' value='解析開始！' style='width:120px;height:50px;'><br><br>
+        <br>
+      </form>
+      <br>
+      <hr color="#363636" size="2">
+      <font style="background-color:#06f5f3;">Copyright &copy; 2021 ActiveTK. All rights reserved.</font>
+    </div>
+  </body>
+</html>
